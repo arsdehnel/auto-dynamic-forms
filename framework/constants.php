3a231a9b9e82056eae533b74078a9f6deaa7e575
arsdehnel <arsdehnel@gmail.com>
2015-03-03 08:09:48 -0600

    Fixed untracked files

--- framework/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-<<<<<<< Updated upstream
-define('CLIENT_PATH','../../../client/');
-=======
-define('CLIENT_PATH','../../../acnmadmin-v2/');
->>>>>>> Stashed changes
-define('JSON_SOURCE','local');
